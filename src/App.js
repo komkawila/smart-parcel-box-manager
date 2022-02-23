05f98958c60c037d735b28c7c1a5b3884ecfb71c
komkawila <kompanuwat@gmail.com>
2022-02-23 19:58:00 +0700

    set layout

--- src/App.js
+++ src/App.js
@@ -7,38 +7,40 @@ import {
   Link
 } from "react-router-dom";
 
+import Nav from './Layout/Nav';
+import Footer from './Layout/Footer';
 export default function App() {
   return (
-    <>
-      <Router>
-        <div>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/dashboard">Dashboard</Link>
-            </li>
-          </ul>
-          <hr />
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-          </Switch>
-        </div>
-      </Router>
-    </>
+    <Router>
+      <div>
+        {/* <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/about">About</Link>
+          </li>
+          <li>
+            <Link to="/dashboard">Dashboard</Link>
+          </li>
+        </ul>
+        <hr /> */}
 
+        <Nav />
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+        <Footer/>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Layout/Nav.js
@@ -0,0 +1,24 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React, { Component } from 'react';
+import logo from '../Assets/img/logo-ssm.png';
+const Nav = () => {
+    return (
+        <nav class="navbar" role="navigation" aria-label="main navigation">
+            <div class="navbar-brand">
+                <a class="navbar-item">
+                    {/* <img src={logo} alt="Bulma: Free, open source, and modern CSS framework based on Flexbox" width="112" height="28"/> */}
+                <h1 class="title">Smart Parcel Box</h1>
+                </a>
+
+                <a role="button" class="navbar-burger" aria-label="menu" aria-expanded="false">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Nav;
