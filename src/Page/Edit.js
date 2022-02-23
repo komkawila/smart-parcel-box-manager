e425e349da23b25e549e130cac7390bfc817b8d0
komkawila <kompanuwat@gmail.com>
2022-02-23 22:54:01 +0700

    /edit user

--- src/Page/Edit.js
+++ src/Page/Edit.js
@@ -5,6 +5,7 @@ import {
 } from "react-router-dom";
 import axios from 'axios';
 import {api} from '../Constants'
+import Swal from 'sweetalert2'
 const Edit = () => {
     const [data,setData] = useState([]);
     const [name,setName] = useState("");
@@ -29,36 +30,72 @@ const Edit = () => {
             }
         });
     },[]);
-     
-    // setData({ ...config_id, config_slope: e.target.value }
-    // useEffect(()=>{
-    //         console.log(`data = `); 
-    //         console.log(data);
-    // },[data]);
-
+    //  setData({ ...data, user_name: e.target.value })
+    //  setData({ ...data, user_email: e.target.value })
+    //  setData({ ...data, user_tel: e.target.value })
+    
+    const saveFunc = () => {
+        Swal.fire({
+            title: 'ยืนยันการบันทึกข้อมูล?',
+            text: "ยืนยันการบันทึกข้อมูล",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#008741',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'ใช่, บันทึกทันที!'
+            }).then((result) => {
+            if (result.isConfirmed) {
+                console.log(data)
+                axios.put(`${api}/api/user/user/id/${data.user_id}`,{
+                    user_name : data.user_name,
+                    user_tel : data.user_tel,
+                    user_email : data.user_email
+                }).then((res)=>{
+                    console.log(`res = `); 
+                    console.log(res.data.data);
+                    if(res.data.data.status){
+                        Swal.fire({
+                            title: 'ยืนยันการบันทึกข้อมูล?',
+                            text: "ยืนยันการบันทึกข้อมูล",
+                            icon: 'success',
+                            confirmButtonColor: '#008741'
+                        });
+                    }else {
+                        Swal.fire({
+                            title: 'บันทึกข้อมูลไม่สำเร็จ?',
+                            text: "บันทึกข้อมูลไม่สำเร็จ",
+                            icon: 'error',
+                            confirmButtonColor: '#008741'
+                        });
+                    }
+                });
+                
+            }
+        });
+    }
     if(waiting)
         return ( 
             <section class="section">
                 <div class="field">
                     <label class="label">ชื่อ</label>
                     <div class="control">
-                        <input class="input" type="text" placeholder="กรุณากรอก ชื่อ-สกุล" value={name} onChange={e => setName(e.target.value)}/>
+                        <input class="input" type="text" placeholder="กรุณากรอก ชื่อ-สกุล" value={data.user_name} onChange={e => setData({ ...data, user_name: e.target.value })}/>
                     </div>
                 </div>
                 <div class="field">
                     <label class="label">อีเมล</label>
                     <div class="control">
-                        <input class="input" type="email" placeholder="กรุณากรอก อีเมล" value={email} onChange={e => setEmail(e.target.value)}/>
+                        <input class="input" type="email" placeholder="กรุณากรอก อีเมล" value={data.user_email} onChange={e => setData({ ...data, user_email: e.target.value })}/>
                     </div>
                 </div>
                 <div class="field">
                     <label class="label">เบอร์โทรศัพท์</label>
                     <div class="control">
-                        <input class="input" type="tel" placeholder="กรุณากรอก เบอร์โทรศัพท์" value={tel} onChange={e => setTel(e.target.value)}/>
+                        <input class="input" type="tel" placeholder="กรุณากรอก เบอร์โทรศัพท์" value={data.user_tel} onChange={e => setData({ ...data, user_tel: e.target.value })}/>
                     </div>
                 </div>
                 <div class="control">
-                    <button class="button is-primary">Submit</button>
+                    <button class="button is-primary" onClick={saveFunc}>บันทึก</button>
                 </div>
             </section>
             
